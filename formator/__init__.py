c6eed5e9dc3d7c6d084fea753362793807704c70
Keveiuz <73086628+Keveiuz@users.noreply.github.com>
2026-02-04 13:23:48 +0800

    Update __init__.py

--- formator/__init__.py
+++ formator/__init__.py
@@ -1,5 +1,10 @@
 from .base import BaseFormator
 from .wildguard import WildGuardFormator, WILDGUARD_TEMPLATE
+from .aegis_v1_split import AegisV1SplitFormator, SPLIT_TEMPLATE
 
-__all__ = ["BaseFormator", "WildGuardFormator", "WILDGUARD_TEMPLATE"]
-
+__all__ = [
+    "BaseFormator", 
+    "WildGuardFormator", 
+    "WILDGUARD_TEMPLATE",
+    "AegisV1SplitFormator",
+    "SPLIT_TEMPLATE"]
